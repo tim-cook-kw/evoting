1143a4dfce2b47b2e64d01bf311f2fb7c26974ca
Saputra <saputra.kmdn@gmail.com>
2019-07-25 18:29:01 +0700

    add db

--- evoting.sql
+++ evoting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Jul 2019 pada 11.12
+-- Waktu pembuatan: 25 Jul 2019 pada 13.28
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.2
 
@@ -36,6 +36,8 @@ CREATE TABLE `calon` (
   `foto` varchar(255) NOT NULL,
   `jenis_kelamin` char(1) NOT NULL,
   `nomor_telepon` varchar(13) NOT NULL,
+  `visi` text NOT NULL,
+  `misi` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -44,9 +46,9 @@ CREATE TABLE `calon` (
 -- Dumping data untuk tabel `calon`
 --
 
-INSERT INTO `calon` (`id`, `nama`, `email`, `alamat`, `foto`, `jenis_kelamin`, `nomor_telepon`, `created_at`, `updated_at`) VALUES
-(5, 'aaaa', 'admin@siakad.com', 'aaa', 'calongambar/59994416_1825752694237503_9101895084525223936_o.jpg', '1', '1233444', '2019-07-11 08:06:15', '2019-07-11 08:06:15'),
-(6, 'aaaa', 'admin@siakad.com', 'aaaaaaaa', 'calongambar/Bca (1).png', '1', '1233444', '2019-07-12 02:14:44', '2019-07-12 02:14:44');
+INSERT INTO `calon` (`id`, `nama`, `email`, `alamat`, `foto`, `jenis_kelamin`, `nomor_telepon`, `visi`, `misi`, `created_at`, `updated_at`) VALUES
+(1, 'Audy', 'guru@guru.com', 'Cibitung', 'calongambar/59994416_1825752694237503_9101895084525223936_o.jpg', '1', '02102101012', 'Jadi Sejahtera', 'Adil Makmur', '2019-07-15 03:43:37', '2019-07-22 04:22:35'),
+(2, 'Panji', 'server.brebes@my.gavilan.edu', 'Tambun', 'calongambar/debate (1).png', '1', '02102101010', 'Maju', 'Terus', '2019-07-15 03:44:27', '2019-07-15 03:44:27');
 
 -- --------------------------------------------------------
 
@@ -81,7 +83,17 @@ CREATE TABLE `pemilih` (
 --
 
 INSERT INTO `pemilih` (`id`, `nik`, `nama`, `jenis_kelamin`, `status_vote`, `created_at`, `updated_at`) VALUES
-(3, 11111111111, 'aaaaa', 1, 0, '2019-07-11 19:42:11', '2019-07-11 19:42:53');
+(2, 123456789012, 'aaaa', 1, 1, '2019-07-13 22:50:46', '2019-07-24 22:26:05'),
+(3, 12345678902, 'aaaa', 1, 1, '2019-07-13 22:51:02', '2019-07-14 02:37:20'),
+(4, 12458765235, 'Saputra Kamandanu', 2, 1, '2019-07-14 20:14:40', '2019-07-22 23:49:00'),
+(26, 1234567890123, 'aaaa', 1, 0, '2019-07-24 20:53:40', '2019-07-24 20:53:40'),
+(29, 1111111111123, 'aaaa', 1, 1, '2019-07-24 20:56:33', '2019-07-25 02:34:09'),
+(30, 111111111116, 'aaaa', 1, 0, '2019-07-24 21:12:46', '2019-07-24 21:12:46'),
+(31, 11111111111693, 'aaaa', 1, 0, '2019-07-24 21:13:38', '2019-07-24 21:13:38'),
+(33, 1111111111198, 'aaaa', 1, 0, '2019-07-24 21:16:31', '2019-07-24 21:16:31'),
+(34, 111111111112345, 'aaaa', 1, 0, '2019-07-24 22:22:38', '2019-07-24 22:25:07'),
+(36, 1111111111123456, 'aaaa', 1, 0, '2019-07-24 22:23:04', '2019-07-24 22:23:04'),
+(40, 111111111112, 'aaaa', 1, 0, '2019-07-24 23:25:50', '2019-07-24 23:25:50');
 
 -- --------------------------------------------------------
 
@@ -100,6 +112,13 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'saputra', 'admin@siakad.com', NULL, '$2y$10$YDtZgxZwYGcbtNQ1JPiSbedA13ydkaE.p0BdPRnmIrNbfZolbMpFS', NULL, '2019-07-13 21:18:24', '2019-07-13 21:18:24');
+
 -- --------------------------------------------------------
 
 --
@@ -113,6 +132,18 @@ CREATE TABLE `vote` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `vote`
+--
+
+INSERT INTO `vote` (`id`, `id_calon`, `id_pemilih`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2019-07-15 00:14:12', '2019-07-15 00:14:12'),
+(2, 1, 6, '2019-07-17 21:01:51', '2019-07-17 21:01:51'),
+(3, 1, 6, '2019-07-17 21:17:39', '2019-07-17 21:17:39'),
+(4, 2, 5, '2019-07-18 21:01:01', '2019-07-18 21:01:01'),
+(5, 1, 4, '2019-07-22 23:48:59', '2019-07-22 23:48:59'),
+(6, 1, 29, '2019-07-25 02:34:09', '2019-07-25 02:34:09');
 
 --
 -- Indexes for dumped tables
@@ -147,9 +178,7 @@ ALTER TABLE `users`
 -- Indeks untuk tabel `vote`
 --
 ALTER TABLE `vote`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_calon` (`id_calon`),
-  ADD KEY `id_pemilih` (`id_pemilih`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -159,7 +188,7 @@ ALTER TABLE `vote`
 -- AUTO_INCREMENT untuk tabel `calon`
 --
 ALTER TABLE `calon`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `migrations`
@@ -171,30 +200,19 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT untuk tabel `pemilih`
 --
 ALTER TABLE `pemilih`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `vote`
 --
 ALTER TABLE `vote`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `vote`
---
-ALTER TABLE `vote`
-  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`id_calon`) REFERENCES `calon` (`id`),
-  ADD CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`id_pemilih`) REFERENCES `pemilih` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
